d3fe1d261549dc438c3ba7d0fad5a8d6a80a16bd
Rashed-Hoque <hoque.rashed@gmail.com>
2017-06-07 00:24:37 +0200

    assembly

---
+++ ch02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ch02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                var methodsCount =0;
+                foreach(var t in a.DefinedTypes)
+                {
+                    methodsCount += t.GetMethods().Count();
+                }
+
+                System.Console.WriteLine($"{a.DefinedTypes.Count()} types with {methodsCount} methods in {r.Name} assembly.");
+            }
+        }
+    }
+}
